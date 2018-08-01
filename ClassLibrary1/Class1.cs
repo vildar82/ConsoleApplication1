9fbec8bd16d2f8c10a450802d83ed9c77744fa87
Vildar <vildar82@gmail.com>
2018-08-01 20:06:20 +0300

    +semver: fix

--- ClassLibrary1/Class1.cs
+++ ClassLibrary1/Class1.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ClassLibrary1
 {
-    public class Class121fhfd
+    public class Class121fhffd
     {
         public string GetInfofb22gr3334 ()
         {
