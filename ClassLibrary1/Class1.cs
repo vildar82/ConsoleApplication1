22b5bf9bc4773e967dd50678bdc8689e9bcc96a7
Vildar <vildar82@gmail.com>
2018-08-01 20:03:01 +0300

    +semver: fix

--- ClassLibrary1/Class1.cs
+++ ClassLibrary1/Class1.cs
@@ -8,7 +8,7 @@ namespace ClassLibrary1
 {
     public class Class121fhfd
     {
-        public string GetInfof22r3334 ()
+        public string GetInfof22gr3334 ()
         {
             throw new NotImplementedException();
         }
